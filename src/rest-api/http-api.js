e0fd641cb372a0a41dc4884c28607303def0c32c
Quentin <ramque@orange.fr>
2024-02-15 02:08:49 +0100

    chage name api > apiRest

--- src/rest-api/http-api.js
+++ src/rest-api/http-api.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
 
-const api = axios.create({
+const apiRest = axios.create({
     baseURL: 'http://127.0.0.1:8000/my_ecom',
     headers: {
       //common['Authorization']: 'AUTH_TOKEN',
     },
     //headers.common['Authorization'] = AUTH_TOKEN;
     //defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    timeout: 2500,
+    timeout: 5000,
 })
 
-export { api }
+export { apiRest }
 
 
