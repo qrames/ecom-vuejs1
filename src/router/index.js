d844cf8ff77114a2b63c78ddd13706ceb5a23309
Quentin <ramque@orange.fr>
2024-02-28 20:18:52 +0100

    Creat Caterogy View

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '@/views/ProductView.vue'
+import CategoryView from '@/views/CategoryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      meta: { transition: 'slide-left' }
     },
     {
       path: '/about',
@@ -22,8 +22,12 @@ const router = createRouter({
     {
       path: '/:category_slug/:product_slug',
       name: 'ProductDetail',
-      component: () => import('../views/ProductView.vue'),
-      meta: { transition: 'slide-right' }
+      component: ProductView
+    {
+      path: '/:category_slug/',
+      name: 'CategoryDetail',
+//    component: () => import('../views/CategoryView.vue'),
+      component: CategoryView,
     },
   ]
 })
---
+++ src/views/CategoryView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import {useRoute} from 'vue-router'
+
+import { apiRest } from '/src/rest-api/http-api'
+
+let latestProducts = ref('')
+
+onMounted( async () => {
+    const response = await apiRest.get('/products' + useRoute().path)
+    console.log(response.data.products)
+    latestProducts.value = response.data.products
+})
+</script>
+
+<template>
+<div class="columns is-multiline main">
+    <div
+    class="column is-3"
+    v-for="product in latestProducts"
+    :key="product.id">
+        <div class="box">
+            <figure class="image mb-4">
+                <img :src="product.get_thumbnail" alt="">
+            </figure>
+            <h3>{{ product.name }}</h3>
+            <p>{{ product.price }}</p>
+            <p>{{ product.description }}</p>
+            <router-link :to="product.get_absolute_url" class="fancy-btn" >
+                detail
+            </router-link>
+        </div>
+    </div>
+</div>
+</template>
+
+<style>
+.main {
+    height: 100%;
+}
+.main div{
+    height: 100%;
+
+}
+
+.fancy-btn {
+  color: #fff;
+  background: linear-gradient(315deg, #42d342 25%, #71ff64);
+  border: none;
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+</style>
