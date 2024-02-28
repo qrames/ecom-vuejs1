453eb6cb041e2b669a0274e3e750a5ba571a9e48
Quentin <ramque@orange.fr>
2024-02-28 20:04:39 +0100

    create ProductBox components

---
+++ src/components/ProductBox.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  product: Object,
+})
+</script>
+
+<template>
+<div class="box">
+    <figure class="image mb-4">
+        <img :src="product.get_thumbnail" alt="">
+    </figure>
+    <h3>{{ product.name }}</h3>
+    <p>{{ product.price }}</p>
+    <p>{{ product.description }}</p>
+    <router-link :to="product.get_absolute_url" class="fancy-btn" >
+        detail
+    </router-link>
+</div>
+</template>
+
+
+<style>
+.main {
+    height: 100%;
+}
+.main div{
+    height: 100%;
+
+}
+
+.fancy-btn {
+  color: #fff;
+  background: linear-gradient(315deg, #42d342 25%, #71ff64);
+  border: none;
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+</style>
--- src/views/LatestProducts.vue
+++ src/views/LatestProducts.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-
 import { apiRest } from '/src/rest-api/http-api'
+
+import ProductBox from '../components/ProductBox.vue';
 
 let latestProducts = ref('')
 
@@ -17,38 +18,7 @@ onMounted( async () => {
     class="column is-3"
     v-for="product in latestProducts"
     :key="product.id">
-        <div class="box">
-            <figure class="image mb-4">
-                <img :src="product.get_thumbnail" alt="">
-            </figure>
-            <h3>{{ product.name }}</h3>
-            <p>{{ product.price }}</p>
-            <p>{{ product.description }}</p>
-            <router-link :to="product.get_absolute_url" class="fancy-btn" >
-                detail
-            </router-link>
-        </div>
+        <ProductBox :product='product'/>
     </div>
 </div>
 </template>
-
-<style>
-.main {
-    height: 100%;
-}
-.main div{
-    height: 100%;
-
-}
-
-.fancy-btn {
-  color: #fff;
-  background: linear-gradient(315deg, #42d342 25%, #71ff64);
-  border: none;
-  padding: 5px 10px;
-  margin: 5px;
-  border-radius: 8px;
-  cursor: pointer;
-}
-
-</style>
