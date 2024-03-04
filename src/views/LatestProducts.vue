d462721e6ff04d5213bf0a0bcbcccea85fc5724b
Quentin <ramque@orange.fr>
2024-03-04 18:42:51 +0100

    loop effect loading and item style

--- src/views/LatestProducts.vue
+++ src/views/LatestProducts.vue
@@ -3,7 +3,6 @@ import { onMounted, ref } from 'vue'
 import { apiRest } from '/src/rest-api/http-api'
 
 import ProductBox from '../components/ProductBox.vue';
-import axios from 'axios';
 
 let latestProducts = ref('')
 
@@ -14,43 +13,46 @@ onMounted( async () => {
 })
 
 async function searchProducts() {
+  document.getElementById("loop").classList.add('fa-pulse')
+  document.getElementById("loop").classList.add('fa-spinner')
+  document.getElementById("loop").classList.remove('fa-search')
 
- try {
+  try {
     const response = await apiRest.get(
       '/latest-products-list/?format=json'
       + '&' + 'query=' + query
     )
-      latestProducts.value = response.data
+    latestProducts.value = response.data
   } catch (err) {
     console.error("Error response:");
     console.error(err.response.data);    // ***
     console.error(err.response.status);  // ***
     console.error(err.response.headers); // ***
   } finally {
-    
+    document.getElementById("loop").classList.remove('fa-pulse')
+    document.getElementById("loop").classList.remove('fa-spinner')
+    document.getElementById("loop").classList.add('fa-search')
   }
 }
 </script>
 
 <template>
-  <div class="columns is-multiline main">
-    <form method="get" action="/latest-products-list/"></form>
+  <form method="get" action="/latest-products-list/">
     <div class="field has-addons">
       <div class="control">
         <input class="input" type="text" placeholder="search" v-model="query">
       </div>
       <div class="control">
         <a class="button is-primary" @click="searchProducts">
-          <i class="fas fa-spinner fa-pulse"></i>
-          <i class="fas fa-search"></i>
+          <i id="loop" class="fas fa-search"></i>
         </a>
       </div>
     </div>
-    <div
-    class="column is-3"
-    v-for="product in latestProducts"
-    :key="product.id">
-        <ProductBox :product='product'/>
-    </div>
+  </form>
+  <div
+  class="gridywrap"
+  v-for="product in latestProducts"
+  :key="product.id">
+      <ProductBox :product='product'/>
   </div>
 </template>
