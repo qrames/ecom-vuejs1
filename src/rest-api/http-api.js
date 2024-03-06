450f09c3180d4807d813dd3e43fca78615156f1b
Quentin <ramque@orange.fr>
2024-03-07 00:39:25 +0100

    settings api

--- src/rest-api/http-api.js
+++ src/rest-api/http-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-
+import { baseURL } from './setting-api'
 
 const apiRest = axios.create({
-    baseURL: 'http://127.0.0.1:8000/my_ecom',
+    baseURL: baseURL + 'my_ecom',
     headers: {
       //common['Authorization']: 'AUTH_TOKEN',
     },
---
+++ src/rest-api/setting-api.js
@@ -0,0 +1,4 @@
+//const baseURL = 'http://127.0.0.1:8000/'
+const baseURL = 'http://192.168.1.18:8000/'
+
+export {baseURL}
